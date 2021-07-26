d48425d21c0101fc0c84812792f8b8f1fe152442
Jordan <jtang0819@gmail.com>
2021-07-26 18:06:35 -0400

    7/26
    update 7/26

--- Python/raw_data_clean.py
+++ Python/raw_data_clean.py
@@ -1,32 +1,46 @@
 from pyspark.sql import SparkSession
 from schema import rawDataSchema, threadDataSchema, socialDataSchema
 
+
+# db_target_properties = {"user":"xxxx", "password":"yyyyy"}
+
+
+# def foreach_batch_function(df, epoch_id):
+#     print(df.collect())
+#     # df.write.jdbc(url='jdbc:mysql://172.16.23.27:30038/securedb',  table="sparkkafka",  properties=db_target_properties)
+#     pass
+
+
 def consume():
     bootstrap_server = "127.0.0.1:9092"
     topicName = "raw_data"
-
     # create spark session
     spark = SparkSession.builder.appName("SparkKafkaConsumer").getOrCreate()
     # create streaming dataframe
-    df = spark \
+    data = spark \
         .readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", bootstrap_server) \
         .option("subscribe", topicName) \
-        .load() \
-        .selectExpr("CAST(value AS STRING)")
-    # .option("startingOffsets", "raw_data") \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    transformed = data.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")\
 
     # select data for sql db
-
-    query = df.writeStream \
-        .format("console") \
+    stream = transformed.writeStream \
+        .format("csv") \
+        .option("format", "append") \
+        .option("path", "checkpointlocation") \
+        .option("checkpointLocation", "checkpointlocation") \
         .queryName("test") \
-        .outputMode("update") \
+        .outputMode("append") \
         .start() \
-        .awaitTermination()
-
-    return query
+        #.awaitTermination()
+    # .foreachBatch(foreach_batch_function) \
+    # clean = stream.select('*')
+    # df = stream.show()
+    return transformed
 
 # consumer = KafkaConsumer(topicName,
 #                          bootstrap_servers=bootstrap_server,
