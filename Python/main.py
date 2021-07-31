2185fdb41a2e7066819f156587218c70fe9ea52a
Jordan <jtang0819@gmail.com>
2021-07-31 03:44:05 -0400

    polishing
    removed clean_data_to_cleandatadb as it already happens.

--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,7 @@
 import raw_data_clean
-import clean_data_to_CleanDataDB
-
-
 
 if __name__ == '__main__':
     print("Begin Run")
     clean = raw_data_clean.consume()
     print("End Run")
 
-# look into multiprocessing
-# how to end the script ie. timeout, sys.exit
