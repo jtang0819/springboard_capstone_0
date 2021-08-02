dc8c9c411b926fb363975ca201574f7fb0cbed48
Jordan <jtang0819@gmail.com>
2021-08-02 19:07:12 -0400

    Update create_tables.sql

--- mySQL/create_tables.sql
+++ mySQL/create_tables.sql
@@ -1,26 +1,26 @@
 CREATE TABLE clean_table (
-master_uuid INT PRIMARY KEY,
-thread VARCHAR(255),
+master_uuid VARCHAR(255) PRIMARY KEY,
 author VARCHAR(255),
+entities_locations VARCHAR(255),
+entities_organizations VARCHAR(255),
+entities_persons VARCHAR(255),
 url VARCHAR(255),
 ord_in_thread INT,
-title VARCHAR(255),
 locations VARCHAR(255),
-entities VARCHAR(255),
-locations VARCHAR(255),
-organizations VARCHAR(255),
-highlightText VARCHAR(255),
-`language` VARCHAR(255),
 persons VARCHAR(255),
-`text` VARCHAR(255),
+organizations VARCHAR(255),
 external_links VARCHAR(255),
+highlightText VARCHAR(255),
+highlightTitle VARCHAR(255),
+`language` VARCHAR(255),
+title VARCHAR(255),
+`text` VARCHAR(255),
 published VARCHAR(255),
-crawled VARCHAR(255),
-highlightTitle VARCHAR(255)
+crawled VARCHAR(255)
 );
 
 CREATE TABLE thread_data (
-master_uuid INT PRIMARY KEY,
+master_uuid VARCHAR(255) PRIMARY KEY,
 site_full VARCHAR(255),
 main_image VARCHAR(255),
 site_section VARCHAR(255),
@@ -40,7 +40,7 @@ uuid VARCHAR(255)
 );
 
 CREATE TABLE social_data (
-master_uuid INT PRIMARY KEY,
+master_uuid VARCHAR(255) PRIMARY KEY,
 gplus_shares INT,
 pinterest_shares  INT,
 vk_shares INT,
@@ -48,5 +48,5 @@ linkedin_shares INT,
 facebook_likes INT,
 facebook_shares INT,
 facebook_comments INT,
-stumbleupon_shares INT
-)
+stumbledupon_shares INT
+);
